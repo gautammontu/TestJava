8844fabd165aa2bdd40923fa5e6419b978edb2ea
Gautam <Blessan@LAPTOP-H6J2RC0D>
2018-07-16 20:07:41 -0700

    new commit

--- JavaGitTest/src/FirstPacakage/Secondprogram.java
+++ JavaGitTest/src/FirstPacakage/Secondprogram.java
@@ -1,5 +1,7 @@
 package FirstPacakage;
 
+import Data_Stracture.DNode;
+
 public class Secondprogram {
 
 	public static void main(String[] args) {
@@ -10,7 +12,74 @@ public class Secondprogram {
 		
 		System.out.println("Added new program");
 		
-	
+		DNode head;
+		DNode first;
+		public DLinkList() {
+			// TODO Auto-generated constructor stub
+		
+		head=null;
+		}
+
+		
+		
+		public void add(Object value){
+			
+			DNode newnode = new DNode(null,value,null);
+			
+			if(head==null){
+				
+				head=newnode;
+				first=newnode;
+			}else{
+				
+				newnode.next=head;
+				head.previous=newnode;
+				head=newnode;
+				
+			}
+			
+		
+	}
+
+	public void display(){
+		
+		DNode n = head;
+		
+		while(n!=null){
+			
+			System.out.println(n.value);
+			n= n.next;
+			
+		}
+	}
+
+		public void displayprevious(){
+			
+			DNode n = head;
+			DNode p= first;
+			/*
+			while(n!=null){
+			
+				
+			System.out.println("basenode:"+n);	
+			n= n.next;
+			if(n!=null){
+				p=n;
+			}   
+			
+			System.out.println("previous:"+p);	
+			} */
+		
+			
+			while(p!=null){
+				
+				System.out.println(p.value);
+				p= p.previous;
+				
+			}
+			
+		
+
 
 	}
 
