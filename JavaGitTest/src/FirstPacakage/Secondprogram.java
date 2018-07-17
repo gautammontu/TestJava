83fe7850dcabee74be289d3bd2ca9be069060b26
Gautam <Blessan@LAPTOP-H6J2RC0D>
2018-07-16 19:49:03 -0700

    first commit from ecillipse

---
+++ JavaGitTest/src/FirstPacakage/Secondprogram.java
@@ -0,0 +1,17 @@
+package FirstPacakage;
+
+public class Secondprogram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Second program");
+
+		
+		System.out.println("Added new program");
+		
+		System.out.println("Added new program from git");
+
+	}
+
+}
---
+++ JavaGitTest/src/FirstPacakage/ThirdProgram.java
@@ -0,0 +1,13 @@
+package FirstPacakage;
+public class ThirdProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Third program");
+		
+	
+		
+	}
+
+}
