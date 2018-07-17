b6d8362452277745b9f07ebcaad404499b43f0d0
Gautam <Blessan@LAPTOP-H6J2RC0D>
2018-07-16 20:03:30 -0700

    new pranch first commit

--- JavaGitTest/src/FirstPacakage/fourthprogram.java
+++ JavaGitTest/src/FirstPacakage/fourthprogram.java
@@ -5,7 +5,7 @@ public class fourthprogram {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("fourth program");
+		System.out.println("hi how r u doing");
 		
 		
 		
