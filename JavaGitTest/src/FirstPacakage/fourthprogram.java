ae119029dd69803ea0c161e43def5df10e8ef79d
Gautam <Blessan@LAPTOP-H6J2RC0D>
2018-07-16 19:54:39 -0700

    New branch 18.5 created and added one file and removed one

--- JavaGitTest/src/FirstPacakage/fourthprogram.java
+++ JavaGitTest/src/FirstPacakage/fourthprogram.java
@@ -1,11 +1,11 @@
 package FirstPacakage;
 
-public class firstprogram {
+public class fourthprogram {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("first program");
+		System.out.println("fourth program");
 		
 		
 		
