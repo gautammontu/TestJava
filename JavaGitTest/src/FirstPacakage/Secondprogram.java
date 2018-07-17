8365d31ba1d5b83c90a4f1bf88360ba5cd027295
Gautam <Blessan@LAPTOP-H6J2RC0D>
2018-07-16 19:58:36 -0700

    updated second program file

--- JavaGitTest/src/FirstPacakage/Secondprogram.java
+++ JavaGitTest/src/FirstPacakage/Secondprogram.java
@@ -5,12 +5,12 @@ public class Secondprogram {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Second program");
+		System.out.println("added new second program");
 
 		
 		System.out.println("Added new program");
 		
-		System.out.println("Added new program from git");
+	
 
 	}
 
